6d28ac39babab9a9a5eaf55eafbee0cbe527aa6c
lianhy <lol_lianhy@163.com>
2019-09-18 17:03:27 +0800

    #201909181703 测试代码 lianhy

--- src/main/java/cn/lianhy/demo/action/HelloWorld.java
+++ src/main/java/cn/lianhy/demo/action/HelloWorld.java
@@ -1,5 +1,6 @@
 package cn.lianhy.demo.action;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,11 +20,17 @@ public class HelloWorld {
     private MongoTemplate mongoTemplate;
 
     @GetMapping(value = "add.json")
-    public String sayHello(){
+    public String add(){
         JSONObject jsonObject=new JSONObject();
-        jsonObject.put("userName","root");
+        jsonObject.put("userName","admin");
         jsonObject.put("password","123456");
-        mongoTemplate.save(jsonObject,"demo");
+        mongoTemplate.save(jsonObject);
         return "Hello：mongo";
+    }
+
+    @GetMapping(value = "get.json")
+    public String get(){
+        List<JSONObject> list=mongoTemplate.findAll(JSONObject.class,"123");
+        return "Hello：mongo"+ JSON.toJSONString(list);
     }
 }
